2fbbb1c1cb31bb21d1c24601ac17c98a86ad8582
Amit Krishna A <54491362+akri16@users.noreply.github.com>
2021-06-05 16:24:45 +0530

    Refactor codebase

--- app.js
+++ app.js
@@ -52,14 +52,6 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 console.log(Date.now())
-// app.use(cookieSession({
-//     maxAge: 24 * 60 * 60 * 1000,
-//     keys: [keys.cookieSession]
-// }));
-
-// const ips = ['172.67.176.16','104.24.123.191','104.24.122.191','10.41.141.207','10.63.249.212','10.69.232.242','108.162.194.81','162.159.38.81','172.64.34.81','172.64.33.140','173.245.59.140','108.162.193.140','162.243.166.170','157.245.130.6']
-// // // Create the server
-// app.use(ipfilter(ips, { mode: 'allow' }))
 
 // initialize passport
 app.use(passport.initialize());
